38298a6bcb452f86a039c7c8351ba7c780495e51
uaman@gmail.com <git100500>
2017-10-01 23:52:36 +0300

    refactor: make User & List mixed components

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,20 +1,42 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom"
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {withRouter, Link} from 'react-router-dom'
+
+import {fetchUsers} from "../../actions/index";
 
 import './list.css';
 
-export class List extends Component {
+class List extends Component {
+
+    componentDidMount() {
+        this.props.dispatch(fetchUsers());
+    }
+
     render() {
         console.log(`arguments:`, arguments);
-        return ( <div class="user-list">
+        return ( <div className="user-list">
             <h3>User list:</h3>
             {this.props.listItems.map(item => (
-                <div>
+                <div key={item.id}>
                     <img src={item.avatar_url} alt="avatar" width="100" height="100"/>
-                    <Link to={`user/${item.id}`}>{item.login}</Link>
+                    <Link to={`user/${item.login}`}>{item.login}</Link>
                     <br/>
                 </div>)
             )}
         </div>);
     }
 }
+
+List.propTypes = {
+    listItems: PropTypes.array.isRequired,
+    dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        listItems: state.list
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(List));
--- src/components/user/index.js
+++ src/components/user/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {withRouter} from 'react-router-dom'
+import {fetchUserData} from "../../actions/index";
 
-export const User = ({match}) => {
-    return (
-        <div>
-            <h3>User accout:</h3>
-            id: {match.params.userId}
-        </div>
-    );
+class User extends Component {
+    componentDidMount() {
+        this.props.dispatch(fetchUserData(this.props.match.params.userName));
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h3>User accout:</h3>
+            </div>
+        );
+    }
 }
+
+User.propTypes = {
+    user: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+
+
+export default withRouter(connect(mapStateToProps)(User));
